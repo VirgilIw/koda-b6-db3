3ec4761a2a6297a8b272493e6f17d597e7f03c1e
Virgil I Ambar <imanuelambar11@gmail.com>
2026-02-23 14:22:07 +0700

    feat: query for 2 exercise join table

---
+++ query2.sql
@@ -0,0 +1,12 @@
+-- join director dan genre ke table movies
+
+select "movies"."id", "movies"."name", "directors"."first_name", "movies_genres"."genre" from "movies"
+join "movies_directors" on "movies_directors"."movie_id" = "movies"."id"
+join "directors" on "directors"."id" = "movies_directors"."director_id"
+join "movies_genres" on "movies_genres"."movie_id" = "movies_directors"."director_id" limit 50 offset 20;
+
+-- join movies dan roles berdasarkan actors
+select "actors"."id", "actors"."first_name", "roles"."role", "movies"."name" from "actors"
+join "roles" on "roles"."actor_id" = "actors"."id"
+join "movies" on "movies"."id" = "roles"."movie_id"
+limit 50 offset 100;
